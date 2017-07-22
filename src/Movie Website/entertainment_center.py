37557292df2a0a24f77b3cd7ed36d1b124eb3b07
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-22 13:38:43 +1000

    Call library fresh tomatoes to show list of movies in html

--- src/Movie Website/entertainment_center.py
+++ src/Movie Website/entertainment_center.py
@@ -1,3 +1,4 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story",
@@ -5,11 +6,20 @@ toy_story = media.Movie("Toy Story",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
-
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster-jpg",
-                     "https://www.youtube.com/watch?=-9ceBgWV8io")
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
 
-print(avatar.storyline)
+school_of_rock = media.Movie("School or Rock",
+                             "Using rock music to learn",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=9PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+                          "A rat is a chef in Paris",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille]
+fresh_tomatoes.open_movies_page(movies)
